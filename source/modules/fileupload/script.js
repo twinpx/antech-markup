8bcb12b6ce0ddd1b03bde848e6883df91930551a
Tatiana <djembuka@gmail.com>
2017-10-10 17:49:07 +0300

    File form

--- source/modules/fileupload/script.js
+++ source/modules/fileupload/script.js
@@ -1,20 +1,36 @@
-// Change this to the location of your server-side upload handler:
-var url = '//jquery-file-upload.appspot.com/';
-
-$('#fileupload').fileupload({
-    url: url,
-    dataType: 'json',
-    done: function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            $('<p/>').text(file.name).appendTo('#files');
-        });
-    },
-    progressall: function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
+if ( $.fn.fileupload ) {
+  $( '#fileupload' )
+  .change( function(e) {
+    var fileName = $(this).val();
+    if ( !(/\.(gif|jpe?g|png|tiff|eps|psd|cdr|pdf)$/i).test( fileName )) {
+      $( '.b-fileupload__ext' ).addClass( 'i-warning' );
+    } else {
+      $( '.b-fileupload__ext' ).removeClass( 'i-warning' );
     }
-}).prop('disabled', !$.support.fileInput)
-    .parent().addClass($.support.fileInput ? undefined : 'disabled');
+  })
+  .fileupload({
+      url: $( '#fileupload' ).closest( '.b-fileupload' ).data( 'url' ),
+      dataType: 'json',
+      acceptFileTypes: /(\.|\/)(gif|jpe?g|png|tiff|eps|psd|cdr|pdf)$/i,
+      done: function (e, data) {
+          $( '.b-fileupload__button-text' ).hide();
+          $.each( data.result.files, function ( index, file ) {
+              $('<div></div>').text(file.name).appendTo('#fileuploadFiles');
+          });
+          $( '.b-fileupload input[name="fileupload"]' ).remove();
+          $( '.b-fileupload' ).append( '<input name="fileupload" type="hidden" value=\'' + JSON.stringify( data.result ) + '\'>' );
+      },
+      progressall: function (e, data) {
+          var progress = parseInt(data.loaded / data.total * 100, 10);
+          $( '#fileUploadProgress .progress-bar' ).css(
+              'width',
+              progress + '%'
+          );
+          setTimeout( function() {
+            $( '#fileUploadProgress' ).addClass( 'i-hide' );
+          }, 300);
+      }
+  })
+  .parent().addClass($.support.fileInput ? undefined : 'disabled')
+  .prop('disabled', !$.support.fileInput);
+}
